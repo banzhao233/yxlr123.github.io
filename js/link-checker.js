20de6fcd89d2295f4c29379ebea830a0b620cfad
yxlr123 <109068213+yxlr123@users.noreply.github.com>
2022-10-02 20:00:38 +0800

    bug Updated By Github Actions

---
+++ js/link-checker.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    checkLink();
+});
+$(document).on('pjax:complete', function () {
+    checkLink();   
+});
+async function checkLink(){
+    console.log("Running...")
+    let link = document.getElementsByTagName('a');
+    //console.log(link)
+    for(var i=0;i<link.length;i++){
+        if(!await checkLocalSite(link[i].href)){
+            link[i].href = "https://go.yxlr.tk/#"+window.btoa(link[i].href)
+            console.log("edit.")
+        }
+    }
+}
+async function checkLocalSite(url){
+    try{
+        console.log("check:",url)
+        let domain = url.split("/")[2];
+        if(domain.endsWith("yxlr.tk")||domain.endsWith("yxlr.tk"))return true;
+        return false;
+    }catch(err){
+    return true;
+    }
+}
