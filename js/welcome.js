2b65628a4e636da5c68835e441d2fa646b3cc405
yxlr123 <109068213+yxlr123@users.noreply.github.com>
2022-10-06 17:31:22 +0800

    gulp压缩 Updated By Github Actions

--- js/welcome.js
+++ js/welcome.js
@@ -1,18 +1,1 @@
-function welcome(){
-    let welcome_text = '欢迎光顾我的博客~'
-    if(document.referrer!==''){
-        let referrer=document.referrer.split("/")[2];
-        welcome_text="欢迎你，来自"+referrer.toUpperCase()+"的用户！";
-        if(referrer.toUpperCase()==document.domain.toUpperCase())return;
-    }
-    swal({
-        title: " 欢迎！",
-        text: welcome_text+'\n打开页面下方音乐以获得更佳体验！',
-        imageUrl: "https://pic.rmb.bdstatic.com/bjh/b3a460ecab97434df969e0aaef39dc53.jpeg",
-        timer: 3000,
-        showConfirmButton: false
-    });
-}
-$(document).ready(()=>{
-    welcome()
-})
+function welcome(){let e="欢迎光顾我的博客~";if(""!==document.referrer){let t=document.referrer.split("/")[2];if(e="欢迎你，来自"+t.toUpperCase()+"的用户！",t.toUpperCase()==document.domain.toUpperCase())return}swal({title:" 欢迎！",text:e+"\n打开页面下方音乐以获得更佳体验！",imageUrl:"https://pic.rmb.bdstatic.com/bjh/b3a460ecab97434df969e0aaef39dc53.jpeg",timer:3e3,showConfirmButton:!1})}$(document).ready((()=>{welcome()}));
