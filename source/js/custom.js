d252de9372cad241af68a363af23c25a5da5fd38
yxlr123 <dbh2858304517@gmail.com>
2022-11-14 19:46:14 +0800

    loading-img

--- source/js/custom.js
+++ source/js/custom.js
@@ -1,10 +1,10 @@
 function owoBig() { let e = 1, t = "", n = document.createElement("div"); n.id = "owo-big"; let i = document.querySelector("body"); i.appendChild(n), document.getElementById("post-comment").addEventListener("DOMNodeInserted", (l => { if (l.target.classList && (l.target.classList.value, 1)) { let o = l.target; o.addEventListener("contextmenu", (e => e.preventDefault())), o.addEventListener("mouseover", (l => { "IMG" == l.target.tagName && e && (e = 0, t = setTimeout((() => { let e = 3 * l.path[0].clientHeight, t = 3 * l.path[0].clientWidth, o = l.x - l.offsetX - (t - l.path[0].clientWidth) / 2; o + t > i.clientWidth && (o -= o + t - i.clientWidth + 10), o < 0 && (o = 10); let d = l.y - l.offsetY; n.style.height = e + "px", n.style.width = t + "px", n.style.left = o + "px", n.style.top = d + "px", n.style.display = "flex", n.innerHTML = `<img src="${l.target.src}">` }), 300)) })), o.addEventListener("mouseout", (i => { n.style.display = "none", e = 1, clearTimeout(t) })) } })) } document.getElementById("post-comment") && owoBig();
-if(PublicSacrificeDay()){
-    document.getElementsByTagName("html")[0].setAttribute("style","filter:gray !important;filter:grayscale(100%);-webkit-filter:grayscale(100%);-moz-filter:grayscale(100%);-ms-filter:grayscale(100%);-o-filter:grayscale(100%);");
-    }
+if (PublicSacrificeDay()) {
+    document.getElementsByTagName("html")[0].setAttribute("style", "filter:gray !important;filter:grayscale(100%);-webkit-filter:grayscale(100%);-moz-filter:grayscale(100%);-ms-filter:grayscale(100%);-o-filter:grayscale(100%);");
+}
 
-function PublicSacrificeDay(){
-    var PSFarr=new Array("0403","0404","0405","0406","0414","0512","0707","0807","0814","0909","0918","0930","1025","1213");
+function PublicSacrificeDay() {
+    var PSFarr = new Array("0403", "0404", "0405", "0406", "0414", "0512", "0707", "0807", "0814", "0909", "0918", "0930", "1025", "1213");
     //2020年4月4日 新冠肺炎哀悼日，清明节
     //2010年4月14日，青海玉树地震
     //2008年5月12日，四川汶川地震
@@ -18,20 +18,20 @@ function PublicSacrificeDay(){
     //1937年12月13日，南京大屠杀
     var currentdate = new Date();
     var str = "";
-    var mm = currentdate.getMonth()+1;
-    if(currentdate.getMonth()>9){
+    var mm = currentdate.getMonth() + 1;
+    if (currentdate.getMonth() > 9) {
         str += mm;
-    }else{
+    } else {
         str += "0" + mm;
     }
-    if(currentdate.getDate()>9){
+    if (currentdate.getDate() > 9) {
         str += currentdate.getDate();
-    }else{
+    } else {
         str += "0" + currentdate.getDate();
     }
-    if(PSFarr.indexOf(str)>-1){
+    if (PSFarr.indexOf(str) > -1) {
         return 1;
-    }else{
+    } else {
         return 0;
     }
 }
@@ -55,31 +55,31 @@ function percent() {
 }
 
 //标签条
-function tagsBarActive(){
-  var urlinfo = window.location.pathname;
-  urlinfo = decodeURIComponent(urlinfo)
-  //console.log(urlinfo);
-  //判断是否是首页
-  if (urlinfo == '/'){
-    if (document.querySelector('#tags-bar')){
-      document.getElementById('首页').classList.add("select")
+function tagsBarActive() {
+    var urlinfo = window.location.pathname;
+    urlinfo = decodeURIComponent(urlinfo)
+    //console.log(urlinfo);
+    //判断是否是首页
+    if (urlinfo == '/') {
+        if (document.querySelector('#tags-bar')) {
+            document.getElementById('首页').classList.add("select")
+        }
+    } else {
+        // 验证是否是分类链接
+        var pattern = /\/tags\/.*?\//;
+        var patbool = pattern.test(urlinfo);
+        //console.log(patbool);
+        // 获取当前的标签
+        if (patbool) {
+            var valuegroup = urlinfo.split("/");
+            //console.log(valuegroup[2]);
+            // 获取当前分类
+            var nowTag = valuegroup[2];
+            if (document.querySelector('#category-bar')) {
+                document.getElementById(nowTag).classList.add("select");
+            }
+        }
     }
-  }else {
-    // 验证是否是分类链接
-    var pattern = /\/tags\/.*?\//;
-    var patbool = pattern.test(urlinfo);
-    //console.log(patbool);
-    // 获取当前的标签
-    if (patbool) {
-      var valuegroup = urlinfo.split("/");
-      //console.log(valuegroup[2]);
-      // 获取当前分类
-      var nowTag = valuegroup[2];
-      if (document.querySelector('#category-bar')){
-        document.getElementById(nowTag).classList.add("select");
-      }
-    }
-  } 
 }
 tagsBarActive()
 
@@ -87,20 +87,20 @@ tagsBarActive()
 window.onscroll = percent; // 执行函数
 // 页面百分比
 function percent() {
-  let a = document.documentElement.scrollTop || window.pageYOffset, // 卷去高度
-    b =
-      Math.max(
-        document.body.scrollHeight,
-        document.documentElement.scrollHeight,
-        document.body.offsetHeight,
-        document.documentElement.offsetHeight,
-        document.body.clientHeight,
-        document.documentElement.clientHeight
-      ) - document.documentElement.clientHeight, // 整个网页高度 减去 可视高度
-    result = Math.round((a / b) * 100), // 计算百分比
-    btn = document.querySelector("#percent"); // 获取图标
+    let a = document.documentElement.scrollTop || window.pageYOffset, // 卷去高度
+        b =
+            Math.max(
+                document.body.scrollHeight,
+                document.documentElement.scrollHeight,
+                document.body.offsetHeight,
+                document.documentElement.offsetHeight,
+                document.body.clientHeight,
+                document.documentElement.clientHeight
+            ) - document.documentElement.clientHeight, // 整个网页高度 减去 可视高度
+        result = Math.round((a / b) * 100), // 计算百分比
+        btn = document.querySelector("#percent"); // 获取图标
 
-  result <= 99 || (result = 99), (btn.innerHTML = result);
+    result <= 99 || (result = 99), (btn.innerHTML = result);
 }
 
 document.getElementById("page-name").innerText = document.title.split(" | 幽香乐容")[0];
